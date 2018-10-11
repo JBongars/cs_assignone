7026d81cbac907b844a8c0a4c923b7c7b70b4e81
Julien Bongars <julienbongars23@gmail.com>
2018-10-11 23:14:24 +0800

    deleted unrelated folders

---
+++ src/main/java/com/cs/assignone/question2/Game.java
@@ -0,0 +1,148 @@
+package com.cs.assignone.question2;
+
+
+import java.util.Arrays;
+
+/**
+ * @Author: Julien Bongars
+ * @Author_id: 234234
+ * @Title: Game class for Question 2 of 2 of Assignment 1
+ * @Description
+ * Game class enables program to start new game. Game state is containerized for easy initialization.
+ */
+class Game {
+
+    /**
+     * @variable secret = the secret phrase
+     * @variable guesses = guesses made throughout the game
+     * @variable wrongAnswerCount = number of wrong guesses made throughout the game
+     * @variaable winFlag = determines the current state of whether the game has been won
+     */
+    private String secret;
+    private String guesses;
+    private int wrongAnswerCount;
+    private boolean winFlag;
+
+    /**
+     * @description constructor
+     * @param secret
+     */
+    public Game (String secret){
+        this.secret = secret;
+        this.guesses = " ";
+        this.wrongAnswerCount = 0;
+        this.winFlag = false;
+    }
+
+    /**
+     * @description Makes a guess on the phrase, if wrong answer, increments wrongAnswerCount
+     * @param guessStr
+     * @mutates wrongAnswerCount
+     * @return score for the turn
+     */
+    public int makeGuess(String guessStr){
+        int score = 0; //init score as 0
+
+        // if a phrase was guessed instead of a single character
+        if(guessStr.length() > 1){
+            if(guessStr.toLowerCase().equals(getSecret())){
+                setWinFlag(true);
+                score = 100 - guesses.length();
+            }
+        } else {
+            char guess = guessStr.charAt(0);
+            System.out.println("New guess = " + guess);
+
+            for(char i: guesses.toCharArray()){
+                if(i == guess){
+                    System.out.println("You have already guessed that character!");
+                    return 0;
+                }
+            }
+
+            this.guesses += guess;
+            if(secret.indexOf(guess) < 0){
+                wrongAnswerCount++;
+            } else if(getPhrase().indexOf('?') >= 0){
+                for(char i: getSecret().toCharArray()){
+                    if(i == guess) score++;
+                }
+                return score;
+            } else {
+                setWinFlag(true);
+                score = 50 - guesses.length();
+            }
+        }
+
+        return score;
+    }
+
+    /**
+     * @description Returns the guesses
+     * @return guesses
+     */
+    public String getGuesses(){
+        String result = guesses;
+        return result;
+    }
+
+    /**
+     * @description Returns the phrase excluding all previous guesses
+     * @return phrase derived from this.secret
+     */
+    public String getPhrase(){
+        String result = secret;
+        return "<" + result.replaceAll("[^" + guesses + "]", "?") + ">";
+    }
+
+    /**
+     * @description get the secret
+     * @return this.secret
+     */
+    public String getSecret() {
+        return secret;
+    }
+
+    /**
+     * @description get the wrong answer count
+     * @return this.wrongAnswerCount
+     */
+    public int getWrongAnswerCount() {
+        return wrongAnswerCount;
+    }
+
+    /**
+     * @description gets win status flag. Whether game has been won
+     * @return winFlag
+     */
+    public boolean getWinFlag() {
+        return winFlag;
+    }
+
+    /**
+     * @description set the secret for the game
+     * @param secret
+     */
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    /**
+     * @description sets win status flag. Whether game has been won
+     * @param winFlag
+     */
+    public void setWinFlag(boolean winFlag) {
+        this.winFlag = winFlag;
+    }
+
+    /**
+     * @description override toString method for this class
+     * @return string "<Game>"
+     */
+    @Override
+    public String toString(){
+        return "<Game>";
+    }
+
+
+}
---
+++ src/main/java/com/cs/assignone/question2/Question2.java
@@ -0,0 +1,117 @@
+package com.cs.assignone.question2;
+
+import com.cs.assignone.dependencies.csScanner;
+import com.cs.assignone.dependencies.csUtility;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @Author: Julien Bongars
+ * @Author_id: 234234
+ * @Title: Question 2 of 2 of Assignment 1
+ * @Description
+ * Program is simple hangman game played by two people
+ * First person will think of a word and insert it into the program
+ * Second person will attempt to guess the work
+ * Program will display hints including characters that were already previous guessed and unknown characters as '?'
+ * Program will display total wrong number of guesses
+ * @Dependencies csScanner (see documentation included in class)
+ */
+public class Question2 {
+
+    public void Question2() { }
+
+    private static csScanner scn = new csScanner();
+
+    public static void main(String[] arg){
+        /**
+         * @variable scn
+         */
+        boolean isPlaying = true;
+        int playerTurn = 1;
+        Game session;
+        Player currentPlayer;
+        List<Player> players = new ArrayList<>();
+
+        csUtility.printStudentInfo("Assignment 1 - Question 2");
+
+        /**
+         * Main Program
+         */
+        while(true){
+
+            /**
+             * Get Player information
+             */
+            do {
+                players.add(new Player(scn.inputStr("Enter player name:")));
+            } while(players.size() < 2 || scn.inputDefaultBool("Add new player?", 'n'));
+
+            /**
+             * Get first player to enter secret phrase (other players will have a turn later on...
+             */
+            do {
+                for (int i = 0; i < players.size(); i++) {
+                    session = new Game(scn.inputStr(players.get(i).getName() + ", please enter the secret phrase:").toLowerCase());
+
+                    /**
+                     * Clear the screen
+                     */
+                    csUtility.clearScreen();
+
+                    /**
+                     * While the game state is not win, give players a chance to guess
+                     */
+                    while (!session.getWinFlag()) {
+                        for (int j = 0; j < players.size(); j++) {
+                            if (i == j) continue; // player who gave secret phrase should not be allowed to guess
+                            currentPlayer = players.get(j);
+
+                            /**
+                             * Displays the phrase mask as ???
+                             */
+                            System.out.println(session.getPhrase());
+                            System.out.println();
+
+                            /**
+                             * Guesses can either be letters or whole phrases
+                             */
+                            currentPlayer.addScore(session.makeGuess(scn.inputStr(currentPlayer.getName() + " please enter a guess:")));
+                            players.set(j, currentPlayer);
+                        }
+                    }
+                    System.out.println("\nGame has been won!\n\n");
+                }
+                /**
+                 * Ask would the players like to play another game.
+                 */
+            } while(scn.inputDefaultBool("Would you like to play another game?", 'y'));
+
+            /**
+             * Scores are tallied up and displayed in descending order
+             */
+            players.sort(Comparator.comparing(Player::getScore, Comparator.reverseOrder()));
+
+            System.out.println(String.format("\n\n%s has won the game! \n\n\n", players.get(0).getName()));
+            System.out.println("-------------------------------");
+            System.out.println("Player      | Score");
+            System.out.println("-------------------------------");
+            for(Player i: players){
+                System.out.println(String.format("%1$s     | %2$d", i.getName(), i.getScore()));
+            }
+            System.out.println("-------------------------------");
+
+            /**
+             * Prompt to restart program. Default value is 'n'
+             */
+            if(!scn.inputDefaultBool("Would you like to restart?", 'n')){
+                break;
+            }
+        }
+        System.out.println("End of Program...");
+
+    }
+
+}
